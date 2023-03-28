7a7a68efb40bffe91f37ee9cf67dd6793f4b7c35
meidhi <meidhi14680@gmail.com>
2023-03-28 14:54:19 +0200

    création styled CardLabel, CardImg

--- shiny-agency/src/components/Card/index.jsx
+++ shiny-agency/src/components/Card/index.jsx
@@ -1,11 +1,23 @@
 import PropTypes from "prop-types";
 import DefaultPicture from "../../assets/profile.png";
+import styled from "styled-components";
 
+const CardLabel = styled.span`
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+const CardImage = styled.img`
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+`;
 function Card({ label, title, picture }) {
   return (
     <div style={{ display: "flex", flexDirection: "column", padding: 15 }}>
-      <span>{label}</span>
-      <img src={picture} alt="freelance" height={80} width={80} />
+      <CardLabel>{label}</CardLabel>
+      <CardImage src={picture} alt="freelance" height={80} width={80} />
       <span>{title}</span>
     </div>
   );
