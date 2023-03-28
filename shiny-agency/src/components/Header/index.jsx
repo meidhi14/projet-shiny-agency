92367b1879840bf2cd6a163a4bdcdbf3f673059b
meidhi <meidhi14680@gmail.com>
2023-03-28 15:26:31 +0200

    AJout des images au projet

--- shiny-agency/src/components/Header/index.jsx
+++ shiny-agency/src/components/Header/index.jsx
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
 
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color:white; border-radius:30px; background-color: ${colors.primary};`}
+`;
 function Header() {
   return (
-    <div>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey/1">Questionnaire</Link>
-      <Link to="/freelances">Freelances</Link>
-    </div>
+    <nav>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/survey/1" $isFullLink>
+        Faire le test
+      </StyledLink>
+      <StyledLink to="/freelances">Freelances</StyledLink>
+    </nav>
   );
 }
 
--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -8,11 +8,19 @@ import Results from "./pages/Results";
 import Freelances from "./pages/Freelances";
 import Header from "./components/Header";
 import Error from "./components/Error";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+div {
+  font-family: 'Trebuchet MS', Helvetica, sans-serif;
+}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
